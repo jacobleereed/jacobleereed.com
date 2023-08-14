918901472e74c591c97d04f66f0f0918696614a5
Jacob Reed <jacob@jacobleereed.com>
2023-08-14 14:22:48 -0400

    gallery

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
+const numOfImages = 82;
+const images = [];
+let currentImage = 0;
 
-let i = 0;
-let images = [];
-let time = 500;
-
-images[0] = '.\resources\sampleshoot\x01.jpg';
-images[1] = '.\resources\sampleshoot\x02.jpg';
-images[2] = '.\resources\sampleshoot\x03.jpg';
-images[3] = '.\resources\sampleshoot\x04.jpg';
-images[4] = '.\resources\sampleshoot\x05.jpg';
-
-console.log(images);
-
-
-function changeImg(){
-    document.slide.src = images[i];
-
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-
-    setTimeout("chaneImg()", time);
+for (let i = 1; i <= numOfImages; i++) {
+    let tempImgURL = './resources/Sample/' + i + '.jpg';
+    images.push(tempImgURL);
 }
 
-document.window.onload = changeImg;
+const img = document.querySelector("img");
+img.src = images[currentImage];
+
+const leftArrowFunc = () => {
+    if (currentImage === 0) {
+        img.src = images[numOfImages -1];
+        currentImage = numOfImages - 1;
+    } else {
+        img.src = images[currentImage--];
+    }
+}
+
+const rightArrowFunc = () => {
+    if (currentImage === (numOfImages)) {
+        img.src = images[0];
+        currentImage = 0;
+    } else {
+        img.src = images[currentImage++];
+    }
+}
+
+
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+
+leftArrow.addEventListener("click", leftArrowFunc);
+rightArrow.addEventListener("click", rightArrowFunc);
 
